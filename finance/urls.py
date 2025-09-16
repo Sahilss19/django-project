463dae825f474221ea4439e4be265e05f02bdb49
Sahil19 <sahilkulria9988@gmail.com>
2025-09-17 01:43:23 +0530

    :)

--- finance/urls.py
+++ finance/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from finance.views import home , HomeView
+# from finance.views import home , HomeView
+from finance.views import registerView
 
 urlpatterns = [
-    path('home/' , home , name = "home"),
-    path('ghar/' , HomeView.as_view () , name = 'ghar') 
+    # path('home/' , home , name = "home"),
+    # path('ghar/' , HomeView.as_view () , name = 'ghar') 
+
+    path('register/' , registerView.as_view(), name = "register")
 
 ]
--- finance/views.py
+++ finance/views.py
@@ -1,19 +1,37 @@
-from django.shortcuts import render , HttpResponse 
+from django.shortcuts import render , HttpResponse , redirect
+from django.shortcuts import render, redirect
 from django.views import View
+from finance.forms import RegisterForm
+from django.contrib.auth import login
 
 
-#function based view
-def home (request):
-    return HttpResponse("heee bro")
+class registerView(View):
+    def get(self, request, *args, **kwargs):
+        form = RegisterForm()
+        return render(request, 'finance/Register.html', {'form': form})
+    
+    def post(self, request, *args, **kwargs):
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('')
+       
 
-#class based view
 
-class HomeView(View):
-    def get(self,request , *args ,**kwargs):
-        return HttpResponse("<h1> Hello from class</h1>")
 
-#for html page 
+# #function based view
+# def home (request):   
+#     return HttpResponse("heee bro")
 
-class HomeView(View):
-    def get(self,request , *args ,**kwargs):
-        return render(request , 'finance/home.html')
+# #class based view
+
+# class HomeView(View):
+#     def get(self,request , *args ,**kwargs):
+#         return HttpResponse("<h1> Hello from class</h1>")
+
+# #for html page 
+
+# class HomeView(View):
+#     def get(self,request , *args ,**kwargs):
+#         return render(request , 'finance/home.html')
